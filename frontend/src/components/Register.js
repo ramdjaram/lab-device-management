f28a488358b8cf080535b25f669959e264495af7
mranisavljevic <milan.ranisavljevic@poly.com>
2024-08-15 22:27:30 +0200

    Add verify password on register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,14 +1,22 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const Register = () => {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [verifyPassword, setVerifyPassword] = useState('');
 	const [role, setRole] = useState('user');
+	const navigate = useNavigate();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		await axios.post('http://localhost:5001/register', {username, password, role})
+		if (password !== verifyPassword) {
+			alert('Passwords do not match');
+			return;
+		}
+		await axios.post('http://localhost:5001/register', {username, password, role});
+		navigate('/login'); // Redirect to login page after successful registration
 	};
 
 	return (
@@ -16,6 +24,8 @@ const Register = () => {
 			<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username"/>
 			<input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
 			       placeholder="Password"/>
+			<input type="password" value={verifyPassword} onChange={(e) => setVerifyPassword(e.target.value)}
+			       placeholder="Verify Password"/>
 			<select value={role} onChange={(e) => setRole(e.target.value)}>
 				<option value="user">User</option>
 				<option value="admin">Admin</option>
