e6f60f4581216b93bed55a1437bf67e7236a2659
mranisavljevic <milan.ranisavljevic@poly.com>
2024-08-15 17:10:41 +0200

    Create components dir

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import Home from './Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
 
 const App = () => {
   return (
