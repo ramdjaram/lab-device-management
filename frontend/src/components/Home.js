1977543d757515116dc39502a48269677e06a1db
mranisavljevic <milan.ranisavljevic@poly.com>
2024-08-16 02:26:06 +0200

    Move css to styles

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-import './Home.css';
+import '../styles/Home.css';
 
 const Home = () => {
 	const [devices, setDevices] = useState([]);
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-import './Login.css';
+import '../styles/Login.css';
 
 const Login = () => {
 	const [username, setUsername] = useState('');
